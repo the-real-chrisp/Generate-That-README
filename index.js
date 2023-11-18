dd289b9662de24e5f9ce61c43b244a7e896b423d
Chris Purcell <chrisp0000@gmail.com>
2023-11-18 12:39:33 -0800

    Added basic structure for inquirer and function to write file

--- index.js
+++ index.js
@@ -1,14 +1,67 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const generateReadMe = ({ title, description, installation, usage, contribution, tests }) =>
+    `# ${title}
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+## Description
+    
+    ${description}
+    
+## Table of Contents
+    
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Credits](#credits)
+    - [License](#license)
+    
+## Installation
+    
+    ${installation}
+    
+## Usage
+    
+## License
+    
+## How to Contribute
+    
+## Tests
+    
+    `;
 
-// TODO: Create a function to initialize app
-function init() {}
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description for your project:'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Describe how users will install your application:'
+        },
+    ])
+    .then((answers) => {
+        const readMeContent = generateReadMe(answers);
 
-// Function call to initialize app
-init();
+        fs.writeFile('README.md', readMeContent, (err) =>
+            err ? console.log(err) : console.log('Successfullly created README.md!')
+        )
+    })
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
